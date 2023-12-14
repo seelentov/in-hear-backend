a708a058fd55569b399c8d1e05e99a4880000126
vladisluw <54843319+seelentov@users.noreply.github.com>
2023-12-14 21:56:38 +0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,8 +1,7 @@
 const CONFIG = {
-	secretJWT: 'kljklJLKSLKJjlk12j3klk12jlkKLQSADJJLL2131l2k3j',
-	mongooseConnectionString:
-		'mongodb+srv://admin:admin@in-hear.jfsmtq2.mongodb.net/in-hear',
-	port: '4444',
+	secretJWT: process.env.SECRET_JWT,
+	mongooseConnectionString:process.env.MONGOOSE_CONNECTION_STRING,
+	port: process.env.PORT || '4444',
   domain: `http://localhost:4444`,
   cors:{
     origin: 'https://in-hear-react.vercel.app'
